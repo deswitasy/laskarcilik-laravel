1fed85a6145f1c2a84fcef16e1b99165aa6f128f
aaiisyah <aaiisyah2007@gmail.com>
2026-06-05 16:06:55 +0700

    fix: pagination

--- resources/views/vendor/pagination/custom.blade.php
+++ resources/views/vendor/pagination/custom.blade.php
@@ -1,46 +1,31 @@
 @if ($paginator->hasPages())
-<nav class="flex items-center justify-center mt-4">
-    <ul class="flex items-center gap-1">
-        {{-- Prev --}}
+<nav class="custom-pagination">
+    <ul>
         @if ($paginator->onFirstPage())
-            <li>
-                <span class="px-3 py-2 text-gray-400 bg-gray-100 rounded cursor-not-allowed">« Prev</span>
-            </li>
+            <li class="disabled"><span>« Prev</span></li>
         @else
-            <li>
-                <a href="{{ $paginator->previousPageUrl() }}" class="px-3 py-2 text-blue-600 bg-white border border-gray-300 rounded hover:bg-blue-50">« Prev</a>
-            </li>
+            <li><a href="{{ $paginator->previousPageUrl() }}">« Prev</a></li>
         @endif
 
-        {{-- Nomor Halaman --}}
         @foreach ($elements as $element)
             @if (is_string($element))
-                <li><span class="px-3 py-2 text-gray-400">{{ $element }}</span></li>
+                <li class="disabled"><span>{{ $element }}</span></li>
             @endif
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <li>
-                            <span class="px-3 py-2 text-white bg-blue-600 rounded font-semibold">{{ $page }}</span>
-                        </li>
+                        <li class="active"><span>{{ $page }}</span></li>
                     @else
-                        <li>
-                            <a href="{{ $url }}" class="px-3 py-2 text-blue-600 bg-white border border-gray-300 rounded hover:bg-blue-50">{{ $page }}</a>
-                        </li>
+                        <li><a href="{{ $url }}">{{ $page }}</a></li>
                     @endif
                 @endforeach
             @endif
         @endforeach
 
-        {{-- Next --}}
         @if ($paginator->hasMorePages())
-            <li>
-                <a href="{{ $paginator->nextPageUrl() }}" class="px-3 py-2 text-blue-600 bg-white border border-gray-300 rounded hover:bg-blue-50">Next »</a>
-            </li>
+            <li><a href="{{ $paginator->nextPageUrl() }}">Next »</a></li>
         @else
-            <li>
-                <span class="px-3 py-2 text-gray-400 bg-gray-100 rounded cursor-not-allowed">Next »</span>
-            </li>
+            <li class="disabled"><span>Next »</span></li>
         @endif
     </ul>
 </nav>
